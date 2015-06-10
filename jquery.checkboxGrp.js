66ca1a4569d9d2ae9054ee9750f466f92b903a31
Krzysztof Grzembski <krzysztof.grzembski@netmedia.com.pl>
2015-06-10 15:01:21 +0200

    Some changes to work with latest jquery

--- jquery.checkboxGrp.js
+++ jquery.checkboxGrp.js
@@ -1,63 +1,63 @@
 (function($){
-	var EVENT_NAME = 'change.mthGroupingPlugin'
+	var EVENT_NAMESPACE = 'mthGroupingPlugin';
+	var EVENT_NAME = 'change.' + EVENT_NAMESPACE;
 
-	var prependChange = function(elem, handler){
-		var el = $(elem)[0];
-		var evcpy = [];
-		var i;
-
-		elem.off(EVENT_NAME);
-		var events = $._data(el, 'events')['change'];
-		for (i=0; i<events.length; i++){
-			evcpy.push(events[i]);
+	var uniqueId = function($element){
+		if ($element.attr('id') === undefined){
+			$element.attr('id', 'chbxuq' + (Math.round(Math.random() * 10000000)) + '_' + (Math.round(Math.random() * 10000000)));
 		}
-
-		elem.off('change');
-		elem.on(EVENT_NAME, handler);
-
-		for (i=0; i<evcpy.length; i++){
-			var name;
-			if(evcpy[i].namespace){
-				name = evcpy[i].type + '.' + evcpy[i].namespace;
-			}else{
-				name = evcpy[i].type;
-			}
-			elem.on(name, evcpy[i].handler);
-		}
+		return $element.attr('id');
 	};
 
 	$.fn.checkboxGrp = function(options){
-
+		var clickedId = null;
 		var grp = this;
+		var o = {changeMethod: 'check'};
+
+		$.extend(o, options);
+
 		grp.each(function(){
+			var $this = $(this);
 
-			var o = {changeMethod: 'check'};
-			$.extend(o, options);
+			$this.off(EVENT_NAME).on(EVENT_NAME, function(ev){
 
-			var $grp = grp;
-			var fn = function(ev){
-
-				$grp.not($(this)).each(function(){
-
-					if ($(this).prop('checked')){
-						if ('click' === o.changeMethod){
-							$(this).off(EVENT_NAME);
-							$(this).trigger('click');
-							prependChange($(this), fn);
-						} else {
-							$(this).prop('checked', false);
-						}
+				if (clickedId === null){
+					clickedId = uniqueId($this);
+				} else {
+					// this event wasn't triggered by clicking on this object
+					// but by clicking other object in this checkboxes group,
+					// so we must skip it
+					if (ev.handleObj.namespace === EVENT_NAMESPACE){
+						return true;
 					}
-				});
-			};
+				}
 
-			if ('click' === o.changeMethod){
-				$(this).off(EVENT_NAME);
-				prependChange($(this), fn);
-			} else {
-				$(this).off(EVENT_NAME).on(EVENT_NAME, fn);
-			}
-			delete grp;
+				grp.each(function(){
+					var l2this = this;
+					var l2$this = $(l2this);
+
+					// Click was on this object anyway, so we should not trigger events second time
+					if (clickedId === uniqueId(l2$this)) { return true; }
+
+					// this element is unchecked so we should skip
+					if (l2$this.prop('checked') === false){ return true; }
+
+					if (o.changeMethod === 'click'){
+						if (o.eventExtraParams){
+							l2$this.trigger('click', o.eventExtraParams);
+						} else {
+							l2$this.trigger('click');
+						}
+
+					}else{
+						l2$this.prop('checked', false);
+					}
+				})
+
+				clickedId = null;
+			});
+
+
 		});
 
 	};
